04c89d512d8434d01274aeb53d499583b85a8f3a
AKASH MAJI <akashmaji946@gmail.com>
2024-02-27 11:10:34 +0530

    Changes for adding splash screen

---
+++ app/src/main/java/me/akashmaji/mycalc/SplashActivity.java
@@ -0,0 +1,31 @@
+package me.akashmaji.mycalc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // create a Thread object for splash thing
+        Thread thread = new Thread() {
+            public void run(){
+                try {
+                    sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        // launch the thread
+        thread.start();
+    }
+}
